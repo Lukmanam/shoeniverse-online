7f2d0676cfdf31f6552a2cce15b625df76baa7be
MUHAMMED LUKMAN A M <129926553+Lukmanam@users.noreply.github.com>
2023-09-09 11:24:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const multer = require("multer");
 // const errorHandler=require('./middleware/errorhandler')
 
 const mongoose = require("mongoose");
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/shoeniverse")
+mongoose
   .connect("mongodb+srv://Lukman:Lukman123@cluster0.wkuk5ce.mongodb.net/shoeniverse")
   .then(() => console.log("Connected to database"))
   .catch(() => console.log("error!! failed to connect database"));
